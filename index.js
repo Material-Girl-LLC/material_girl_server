7715dcbfeb91a8ca151ee3bb0492bbc48d590423
Mathew Schwartzman <mathew.schwartzman@whoi.edu>
2023-12-17 22:00:09 -0500

    feat: adds service files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const materials_routes = require('./routes/materials');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: 3030,
         host: 'localhost'
     });
 
